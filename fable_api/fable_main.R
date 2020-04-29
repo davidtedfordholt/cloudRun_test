1e2a0cb07a3673aed1ea0b69954751dfebd68cca
David Holt <d@tdfd.org>
2020-04-29 15:46:44 -0400

    fix fable_main.R to use port 8000 and tsbl

--- fable_api/fable_main.R
+++ fable_api/fable_main.R
@@ -1,12 +1,14 @@
+library(plumber)
+
 packageStartupMessage("Starting plumber")
-api <- plumber::plumb("fable_api.R")
+api <- plumber::plumb("fable_api/plumber.R")
 
 packageStartupMessage("filtering object")
-api$filter("robj", function(req) {
+api$filter("tsbl", function(req) {
     req$rook.input$rewind()
-    req$robj <- unserialize(req$rook.input$read())
+    req$tsbl <- unserialize(req$rook.input$read())
     plumber::forward()
 })
 
 packageStartupMessage("running plumber")
-api$run(debug = TRUE, port = Sys.getenv("PORT"))
+api$run(debug = TRUE, port = 8000)
